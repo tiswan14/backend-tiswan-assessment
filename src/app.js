7e491031553384fa466c99bce77f9db02b0fe6e2
Tiswan <109360038+tiswan14@users.noreply.github.com>
2025-10-14 22:05:27 +0700

    feat(user): implement full user management endpoints
    This commit introduces a complete user management module, including:
    -  to retrieve all users (Admin only)
    -  to fetch a single user by ID (Admin & Manager)
    -  to delete a user (Admin only)
    
    Additional improvements:
    - Added repository and service layers for clean separation of concerns.
    - Integrated Prisma transactions for safe user deletion.
    - Configured  in RefreshToken model to auto-remove tokens.
    - Ensured error handling consistency with global error middleware.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
+import userRoutes from './routes/user.routes.js'
 import taskRoutes from './routes/task.route.js'
 import attachmentRoutes from './routes/attachment.route.js'
 import { errorHandler } from './middlewares/error.handler.js'
@@ -12,6 +13,8 @@ const app = express()
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
+
+app.use('/api', userRoutes)
 
 app.use('/api', taskRoutes)
 
