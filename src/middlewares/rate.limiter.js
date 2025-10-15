6647db625f330d5de6946c6fa48bd50144c9bf70
Tiswan <109360038+tiswan14@users.noreply.github.com>
2025-10-15 12:19:12 +0700

    Revert "docs(api): add filter examples and count field to GET /api/tasks section" (#13)
    This reverts commit 3773ef5617479967f0f97b0e04eaf4bfa06c9359.

--- src/middlewares/rate.limiter.js
+++
@@ -1,9 +0,0 @@
-import rateLimit from 'express-rate-limit'
-
-const rateLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 10,
-    message: { message: 'Too many requests. Try again later.' },
-})
-
-export default rateLimiter
