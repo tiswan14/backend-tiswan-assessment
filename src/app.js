0b858a2f371fdd05a73d71f92927a4684df72487
Tiswan <109360038+tiswan14@users.noreply.github.com>
2025-10-15 12:18:01 +0700

    Revert "docs(api): add filter examples and count field to GET /api/tasks section"
    This reverts commit 3773ef5617479967f0f97b0e04eaf4bfa06c9359.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,5 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import authRoutes from './routes/auth.route.js'
-import userRoutes from './routes/user.routes.js'
-import taskRoutes from './routes/task.route.js'
-import attachmentRoutes from './routes/attachment.route.js'
-import { errorHandler } from './middlewares/error.handler.js'
 
 dotenv.config()
 
@@ -12,20 +7,8 @@ const app = express()
 
 app.use(express.json())
 
-app.use('/api/auth', authRoutes)
-
-app.use('/api', userRoutes)
-
-app.use('/api', taskRoutes)
-
-app.use('/api', attachmentRoutes)
-
 app.get('/', (req, res) => {
-    res.json({
-        message: '🚀 Welcome to Task Management API',
-    })
+    res.json({ message: 'Hello World from Express!' })
 })
-
-app.use(errorHandler)
 
 export default app
--- src/routes/attachment.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { handleDeleteAttachment } from '../controllers/attachment.controller.js'
-import { authenticateToken } from '../middlewares/auth.middleware.js'
-
-const router = express.Router()
-
-router.delete('/attachments/:id', authenticateToken, handleDeleteAttachment)
-
-export default router
